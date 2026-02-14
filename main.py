78b0f960645fe5d964a4489f14e328a419e2c7e9
Lucas R Da Silva <lucasrdasilva@MBP-de-Lucas.lan>
2026-02-14 10:50:28 -0800

    Corrigindo erros

--- main.py
+++ main.py
@@ -59,7 +59,7 @@ def put_livros(id_livro: int, livro: Livro):
     if not meu_livro:
         raise HTTPException(status_code=404, detail="Esse livro não foi encontrado!")
     else:
-        meu_livro[id_livro] = livro.dict()   
+        meus_livrozinhos[id_livro] = livro.dict()   
         return {"message": "As informações do seu livro foram atualizadas com sucesso!"}
     
 
