0b2b53120d1970cf9228bc64a04cb5f9427411e9
Lucas R Da Silva <lucasrdasilva@MacBook-Pro-de-Lucas.local>
2026-05-04 08:55:53 -0700

    Tarefa modulo 32

--- tasks.py
+++ tasks.py
@@ -22,3 +22,50 @@ def fatorial(self, n):
 # 1 - Vamos criar algumas tarefas
 # 2 - Vamos rodar essas tarefas em background usando o Celery
 # 3 - Vamos jogar essas  tarefas para o Redis usando-o como sistema de fila
+
+
+#test_database.py
+
+# import pytest
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker
+# from main import Base, LivroDB, app
+# from fastapi.testclient import TestClient
+# from main import Usuario
+# import os
+
+# DATABASE_URL_TEST = "sqlite:///:memory:"
+# engine = create_engine(DATABASE_URL_TEST, connect_args={"check_same_thread": False})
+# TestingSessionLocal = sessionmaker(bind=engine)
+
+# Base.metadata.create_all(bind=engine)
+
+# client = TestClient(app)
+
+# @pytest.fixture(autouse=True)
+# def mock_redis(mocker):
+    # mock_redis_client = mocker.patch("main.redis_client", autospec=True)
+    # mock_redis_client.get.return_value = None
+
+# @pytest.fixture(scope="function")
+# def db():
+    # db = TestingSessionLocal()
+    # try:
+        # yield db
+    # finally:
+        # db.close()
+
+# def test_get_books(db, mocker):
+    # admin_user = Usuario(username="admin", password="admin")
+    # db.add(admin_user)
+    # db.commit()
+    
+    # response = client.get("/livros", auth=("admin", "admin"))
+    # assert response.status_code == 200
+
+    # data = response.json()
+
+    # assert len(data["livros"]) == 10
+    # assert data["livros"][0]["nome_livro"] == "A Revolução dos Bichos"
+    # assert data["livros"][0]["autor_livro"] == "George Orwell"
+    # assert data["livros"][0]["ano_livro"] == 1945
---
+++ tests/test_tasks.py
@@ -0,0 +1,13 @@
+from tasks import somar, fatorial
+
+def test_somar():
+    resultado = somar.apply(args=[5, 3]).get()
+    assert resultado == 8
+
+def test_fatorial():
+    resultado = fatorial.apply(args=[5]).get()
+    assert resultado == 120
+
+def test_fatorial_zero():
+    resultado = fatorial.apply(args=[0]).get()
+    assert resultado == 1 
